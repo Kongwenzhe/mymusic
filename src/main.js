f9ebc2eadff23c590cc19cfd5bd0e9b4eef67fe4
Kongwenzhe <30457359+Kongwenzhe@users.noreply.github.com>
2018-03-18 17:01:22 +0800

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,16 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+import fastclick from 'fastclick'
+
+fastclick.attach(document.body)
+
+import 'common/stylus/index.styl'
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
